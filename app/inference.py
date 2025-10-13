f888757f63f0a81a30355918c4f60552ed79e10c
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-10-13 17:48:09 +0530

    Deployement

---
+++ app/inference.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+kmeans = joblib.load(r"C:\Users\adity\customer_segmentation_project\models\kmeans_model.pkl")
+scaler = joblib.load(r"C:\Users\adity\customer_segmentation_project\models\scaler.pkl")
+
+st.title("Customer Segmentation App")
+st.write("Enter a customer details to predict the segment")
+
+#"Age", "Income", 'TotalSpend', 'NumWebPurchases' , 'NumStorePurchases',  'NumWebVisitsMonth', 'Recency'
+age = st.number_input("Age", min_value=18,max_value=100, value=35)
+income = st.number_input("Income", min_value=0, max_value=200000,value=50000)
+total_spend = st.number_input("TotalSpend", min_value=0, max_value=5000, value=1000)
+num_web_purchases = st.number_input("NumWebPurchases", min_value=0, max_value=100, value=10)
+num_store_purchases = st.number_input("NumStorePurchases", min_value=0, max_value=100, value=10)
+num_web_visits_month = st.number_input("NumWebVisitsMonth",min_value=0, max_value=50, value=3)
+recency = st.number_input("Recency",min_value=0, max_value=365, value=30)
+
+
+
+input_data = pd.DataFrame(
+    {
+        "Age":[age],
+        "Income":[income],
+        "TotalSpend" : [total_spend],
+        "NumWebPurchases" : [num_web_purchases],
+        "NumStorePurchases" : [num_store_purchases],
+        "NumWebVisitsMonth" : [num_web_visits_month],
+        "Recency" : [recency]
+    }
+)
+
+input_scaled = scaler.transform(input_data)
+
+if st.button("Predict Segment "):
+    clusters = kmeans.predict(input_scaled)[0]
+    st.success(f"Predicted Segment : Cluster {clusters}")
+    
+               
+
---
+++ src/model_training.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import os
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import joblib
+
+
+
+PROCESSED_PATH = Path(r"C:\Users\adity\customer_segmentation_project\data\processed\cleaned_data.csv")
+df = pd.read_csv(PROCESSED_PATH)
+
+
+features = ["Age", "Income", 'TotalSpend', 'NumWebPurchases' , 'NumStorePurchases',  'NumWebVisitsMonth', 'Recency']
+
+x=df[features].copy()
+
+scaler=StandardScaler()
+x_scaled=scaler.fit_transform(x)
+
+wcss = []
+
+for i in range(2,10):
+    kmeans=KMeans(n_clusters=i)
+    kmeans.fit(x_scaled)
+    wcss.append(kmeans.inertia_)
+
+
+joblib.dump(kmeans, r"C:\Users\adity\customer_segmentation_project\models\kmeans_model.pkl")
+joblib.dump(scaler, r"C:\Users\adity\customer_segmentation_project\models\scaler.pkl")
+
